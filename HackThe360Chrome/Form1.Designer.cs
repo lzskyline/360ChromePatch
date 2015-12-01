9bd2e44e07c79f9f625eb1df052f01712d59262c
LzSkyline <lzskyline@foxmail.com>
2015-12-01 19:08:36 +0800

    Binary file read&write

---
+++ HackThe360Chrome/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace HackThe360Chrome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "One Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 216);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(304, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoToolTip = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(68, 17);
+            this.toolStripStatusLabel1.Text = "未打开文件";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 101);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "说明";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 79);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "本程序可以将 360极速浏览器 中的 选中搜索引擎 由 好搜 替换为 百度\r\n\r\n仅供学习参考使用.\r\nA small routine for learning " +
+    "C# , programming by LzSkyline.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Chrome.Dll|chrome.dll";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(304, 238);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hack The 360Chrome";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ HackThe360Chrome/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HackThe360Chrome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr;
+            StreamWriter sw;
+            FileStream fo;
+            string rs="";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName == "") return;
+            try {
+                fo = new FileStream(openFileDialog1.FileName,FileMode.OpenOrCreate,FileAccess.ReadWrite);
+                
+                sr = new StreamReader(fo, Encoding.ASCII);
+                sw = new StreamWriter(fo,Encoding.ASCII);
+            }
+            catch (IOException err)
+            {
+                MessageBox.Show(err.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                while(!sr.EndOfStream)
+                {
+                    Application.DoEvents();
+                    rs = sr.ReadLine();
+                    toolStripStatusLabel1.Text = "Searching:" + sr.BaseStream.Position.ToString() + "/" + sr.BaseStream.Length;
+                    if (rs.IndexOf("haosou.com/s?q")!=-1)
+                    {
+                        sw.BaseStream.Position = sr.BaseStream.Position - rs.Length;
+                        sw.Write(rs.Replace("haosou.com/s?q", "baidu.com/s?wd"));
+                        sw.Flush();
+                        fo.Flush();
+                        sw.Close();
+                        toolStripStatusLabel1.Text = "已完成修改!";
+                        MessageBox.Show("Cracked!", "patch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        button1.Enabled = false;
+                        return;
+                    }
+                    
+                }
+                toolStripStatusLabel1.Text = "所选文件不对或该文件已被补丁!";
+                MessageBox.Show("Failed!", "patch", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (IOException err)
+            {
+                toolStripStatusLabel1.Text = err.Message;
+                MessageBox.Show(err.Message,"错误" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            sw.Close();
+        }
+    }
+}
